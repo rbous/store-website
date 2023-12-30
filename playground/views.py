e12aaee7c9acc3158108d687a5686a752723a401
rbous <rboussanni@gmail.com>
2023-12-29 20:51:15 -0500

    right before introduction of databases

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
-    return render(request, 'hello.html')
+    return render(request, 'hello.html', {'name': 'Rayane'})
+
+def home_page(request):
+    return HttpResponse("<h1>Home page</h1>")
--- storefront/urls.py
+++ storefront/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from playground import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path('playground/', include('playground.urls')),
+    path('', views.home_page),
 ]
